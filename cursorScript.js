9019e23c929832c11dc03913ef3d57fdf196bfd9
grountish <grountish@gmail.com>
2022-11-26 11:01:05 +0100

    pop up

---
+++ cursorScript.js
@@ -0,0 +1,36 @@
+const imagesSrc = [
+    {
+      srcDefault: "section1-img1.png",
+      srcHover: "section1-img2.png",
+    },
+    {
+        srcDefault: "section2-img1.png",
+        srcHover: "section2-img2.png",
+      },
+      {
+        srcDefault: "section3-img1.png",
+        srcHover: "section3-img2.png",
+      },
+      {
+        srcDefault: "section4-img1.png",
+        srcHover: "section4-img2.png",
+      },
+      {
+        srcDefault: "section5-img1.png",
+        srcHover: "section5-img2.png",
+      },
+      {
+        srcDefault: "section6-img1.png",
+        srcHover: "section6-img2.png",
+      },
+      {
+        srcDefault: "section7-img1.png",
+        srcHover: "section7-img2.png",
+      },
+      {
+        srcDefault: "section8-img1.png",
+        srcHover: "section8-img1.png",
+      }
+  ];
+  const container = document.getElementById("firstSection");
+  const img = document.getElementById("image");
